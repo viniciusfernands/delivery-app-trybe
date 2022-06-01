ee55083f2394129b366eb1134fedd5efc3f1cb86
BrenoRCBrito <brenorcbrito@gmail.com>
2022-06-01 16:06:35 -0300

    feat[middleware]: add authentication middleware :sparkles:

---
+++ back-end/src/middlewares/auth.js
@@ -0,0 +1,22 @@
+const { decode } = require('../utils/jwt');
+
+const auth = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(400).json({
+      message: 'Token not found',
+    });
+  }
+  try {
+    const decoded = decode(token);
+    req.user = decoded.payload;
+    return next();
+  } catch (err) {
+    console.error(err);
+    return res.status(401).json({
+      message: 'Unauthorized',
+    });
+  }
+};
+
+module.exports = auth;

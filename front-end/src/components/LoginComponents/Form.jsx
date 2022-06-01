7296b0f318b28f94e83972169b1ceee1e63ac48f
viniciusfernands <viniciusfernandes126@gmail.com>
2022-06-01 17:04:58 -0300

    feat: add validations for inputs

--- front-end/src/components/LoginComponents/Form.jsx
+++ front-end/src/components/LoginComponents/Form.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function LoginForm() {
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+
+  const validateInputs = () => {
+    const regex = /\S+@\S+\.[a-zA-Z]+/;
+    const validEmail = regex.test(userEmail);
+    const SIX = 6;
+
+    if (validEmail && userPassword.length >= SIX) {
+      return false;
+    }
+    return true;
+  };
+
+  const handleInputLogin = ({ target }) => {
+    setUserEmail(target.value);
+  };
+
+  const handleInputPassword = ({ target }) => {
+    setUserPassword(target.value);
+  };
+
   return (
     <div>
       <img alt="imagem do nosso logo" />
@@ -11,6 +33,8 @@ function LoginForm() {
             id="login-id"
             placeholder="Digite seu email"
             data-testid="common_login__input-email"
+            value={ userEmail }
+            onChange={ (event) => handleInputLogin(event) }
           />
         </label>
 
@@ -20,12 +44,15 @@ function LoginForm() {
             id="password-id"
             placeholder="Digite sua senha"
             data-testid="common_login__input-password"
+            value={ userPassword }
+            onChange={ (event) => handleInputPassword(event) }
           />
         </label>
 
         <button
           type="submit"
           data-testid="common_login__button-login"
+          disabled={ validateInputs() }
         >
           LOGIN
         </button>

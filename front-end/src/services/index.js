c05c503e7e118e052e469f9417bdaf2077d603dd
viniciusfernands <viniciusfernandes126@gmail.com>
2022-06-01 19:16:30 -0300

    fix: server connection error

--- front-end/src/services/index.js
+++ front-end/src/services/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default function postLogin(email, password) {
-  return axios.post('http://localhost:3333/login', {
+  return axios.post('http://localhost:3001/login', {
     email,
     password,
   })

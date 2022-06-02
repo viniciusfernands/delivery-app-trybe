a6f5f1c483d0f79cb05e770d598196335c94c9ab
pro gui <ggiacomini2012@gmail.com>
2022-06-02 15:36:11 -0300

    feat: create CustomerProduct page and add it to page index

---
+++ front-end/src/pages/CustomerProduct/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import LoginForm from '../../components/LoginComponents/Form';
+
+function CustomerProducts() {
+  return (
+    <h1>CustomerProductsPage</h1>
+  );
+}
+
+export default CustomerProducts;
--- front-end/src/pages/index.jsx
+++ front-end/src/pages/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Login from './Login';
+import CustomerProduct from './CustomerProduct';
 
 function Pages() {
   return (
     <Switch>
       <Route exact path="/login" component={ Login } />
+      <Route exact path="/customer/products" component={ CustomerProduct } />
       <Route render={ () => <Redirect to={ { pathname: '/login' } } /> } />
     </Switch>
   );

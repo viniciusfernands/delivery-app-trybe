30eb874ec8e12b361a1297879b894b9b982308ad
viniciusfernands <viniciusfernandes126@gmail.com>
2022-06-02 16:53:56 -0300

    chore: change route

---
+++ front-end/src/components/utils/inputsDatas.jsx
@@ -0,0 +1,34 @@
+const data = {
+  Login: {
+    id: 'login-id',
+    placeholder: 'Digite seu email',
+    testId: 'common_login__input-email',
+    role: 'Login',
+  },
+  Password: {
+    id: 'password-id',
+    placeholder: 'Digite sua senha',
+    testId: 'common_login__input-password',
+    role: 'Senha',
+  },
+  Email: {
+    id: 'email-id',
+    placeholder: 'Digite seu email',
+    testId: 'common_register__input-email',
+    role: 'Email',
+  },
+  Name: {
+    id: 'name-id',
+    placeholder: 'Seu nome',
+    testId: 'common_register__input-name',
+    role: 'Nome',
+  },
+  RegisterPassword: {
+    id: 'password-id',
+    placeholder: '**********',
+    testId: 'common_register__input-password',
+    role: 'Senha',
+  },
+};
+
+export default data;

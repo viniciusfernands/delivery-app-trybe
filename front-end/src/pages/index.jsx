28e9c09ca11f473a7d8140bb98c6d083284908d4
viniciusfernands <viniciusfernandes126@gmail.com>
2022-06-02 16:55:04 -0300

    feat: req 6 - Ok

--- front-end/src/pages/index.jsx
+++ front-end/src/pages/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Login from './Login';
+import Register from './Register';
 import CustomerProduct from './CustomerProduct';
 
 function Pages() {
   return (
     <Switch>
       <Route exact path="/login" component={ Login } />
+      <Route exact path="/register" component={ Register } />
       <Route exact path="/customer/products" component={ CustomerProduct } />
       <Route render={ () => <Redirect to={ { pathname: '/login' } } /> } />
     </Switch>

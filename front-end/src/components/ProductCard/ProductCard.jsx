fa18a3ed9f775a9d37374e0ebf9168c40b0a95ab
Tiago <sathler@gmail.com>
2022-06-03 16:46:48 -0300

    feat[front-req11]: req11 without req12 - no tests

---
+++ front-end/src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,61 @@
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+
+function ProductCard(props) {
+  const { product: { id, name, price, urlImage } } = props;
+  const [quantity, setQuantity] = useState(0);
+
+  const priceBR = price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+
+  return (
+    <div>
+      <p
+        data-testid={ `customer_products__element-card-title-${id}` }
+      >
+        { name }
+      </p>
+      <p
+        data-testid={ `customer_products__element-card-price-${id}` }
+      >
+        { priceBR }
+      </p>
+      <img
+        data-testid={ `customer_products__element-card-image-${id}` }
+        src={ urlImage }
+        alt={ `cerveja ${name}` }
+      />
+      <button
+        data-testid={ `customer_products__button-card-add-item-${id}` }
+        type="button"
+        onClick={ () => setQuantity(quantity + 1) }
+      >
+        +
+      </button>
+      <p
+        data-testid={ `customer_products__input-card-quantity-${id}` }
+      />
+      <button
+        data-testid={ `customer_products__button-card-rm-item-${id}` }
+        type="button"
+        onClick={ () => { if (quantity > 0) setQuantity(quantity - 1); } }
+      >
+        -
+      </button>
+
+    </div>
+  );
+}
+
+ProductCard.propTypes = {
+  product: PropTypes.shape(
+    PropTypes.object({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      price: PropTypes.number,
+      urlImage: PropTypes.string,
+    }),
+  ).isRequired,
+
+};
+
+export default ProductCard;
---
+++ front-end/src/components/ProductsList/ProductsList.jsx
@@ -0,0 +1,21 @@
+// import PropTypes from 'prop-types';
+import React, { useContext } from 'react';
+import ProductCard from '../ProductCard/ProductCard';
+import Context from '../../context/Context';
+
+function ProductsList() {
+  const { products } = useContext(Context);
+  return (
+    <div>
+      {
+        products.map((product, index) => (
+          <div key={ index }>
+            <ProductCard product={ product } />
+          </div>
+        ))
+      }
+    </div>
+  );
+}
+
+export default ProductsList;

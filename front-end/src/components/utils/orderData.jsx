a01bc3b8986bafa23df51776f806b085b9c2c3ac
viniciusfernands <viniciusfernandes126@gmail.com>
2022-06-06 17:49:55 -0300

    feat: add Seller data

--- front-end/src/components/utils/orderData.jsx
+++ front-end/src/components/utils/orderData.jsx
@@ -7,7 +7,15 @@ const data = {
     date: 'customer_orders__element-order-date-',
     price: 'customer_orders__element-card-price-',
   },
-  Seller: {},
+  Seller: {
+    role: 'seller',
+    html: 'order-',
+    id: 'seller_orders__element-order-id-',
+    status: 'seller_orders__element-delivery-status-',
+    date: 'seller_orders__element-order-date-',
+    price: 'seller_orders__element-card-price-',
+    address: 'seller_orders__element-card-address-',
+  },
 };
 
 export default data;

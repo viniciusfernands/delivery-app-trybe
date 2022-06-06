ecba9a5275f5fe6d84b60ee747da60f18a630abf
pro gui <ggiacomini2012@gmail.com>
2022-06-06 18:25:49 -0300

    fix: add package-locks to commit and change delete.sh to only delete package-locks qhen executed

--- misc/delete.sh
+++ misc/delete.sh
@@ -1,13 +1,13 @@
 cd ./back-end
 rm -rf package-lock.json
-rm -rf node_modules
-echo 'if back-end package-lock or node_modules exists, now they are deleted\n'
+# rm -rf node_modules
+echo 'if back-end package-lock exists, now its deleted\n'
 cd ..
 cd ./front-end
 rm -rf package-lock.json
-rm -rf node_modules
-echo 'if front-end package-lock or node_modules exists, now they are deleted\n'
+# rm -rf node_modules
+echo 'if front-end package-lock exists, now its deleted\n'
 cd ..
 rm -rf package-lock.json
-rm -rf node_modules
-echo 'if rootDir package-lock or node_modules exists, now they are deleted\n'
+# rm -rf node_modules
+echo 'if rootDir package-lock exists, now its deleted\n'

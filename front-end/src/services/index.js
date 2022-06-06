bdd59767a2beeb2b0d5ecc964db25ab63df9d0bc
pro gui <ggiacomini2012@gmail.com>
2022-06-06 20:18:34 -0300

    fix: merging problems

--- front-end/src/services/index.js
+++ front-end/src/services/index.js
@@ -28,25 +28,21 @@ function getProducts(token) {
     .catch((err) => err.response.status);
 }
 
-<<<<<<< HEAD
 function getSales(token) {
   return axios
-    .get('http://localhost:3001/sale', { headers: { Authorization: token } })
-=======
+    .get('http://localhost:3001/sale', { headers: { Authorization: token } });
+}
+
 function getOrders(token) {
   return axios.get('http://localhost:3001/orders', { headers: { Authorization: token } })
->>>>>>> eb2af964a3061cebd15c9bab731e15b542b59fa0
     .then((res) => res.data)
     .catch((err) => err.response.status);
 }
 
-<<<<<<< HEAD
-export { postLogin, postRegister, getProducts, getSales };
-=======
 export {
   postLogin,
   postRegister,
   getProducts,
   getOrders,
+  getSales,
 };
->>>>>>> eb2af964a3061cebd15c9bab731e15b542b59fa0

64605a6c4c733bd5c76a5b201a690df95923e488
viniciusfernands <viniciusfernandes126@gmail.com>
2022-06-08 16:00:43 -0300

    feat: add navbar in order details

--- front-end/src/pages/OrderDetails/index.jsx
+++ front-end/src/pages/OrderDetails/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
 import OrderDetail from '../../components/OrderDetail';
 
 function OrderDetails() {
   return (
     <div>
+      <Navbar />
       <h1>Detalhes do Pedido</h1>
       <OrderDetail />
     </div>

50680d949f1fb8c2e3edd1691615f866cdced395
Tiago <sathler@gmail.com>
2022-06-09 16:54:28 -0300

    :sparkles: feat[Admin page]: add useEffect, useState, useContext, initialize user and get users

--- front-end/src/pages/Admin/index.jsx
+++ front-end/src/pages/Admin/index.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import AdminForm from '../../components/AdminComponents/Form';
 import Navbar from '../../components/Navbar/Navbar';
 import AdminUsersTable from '../../components/AdminUsersTable/AdminUsersTable';
+import Context from '../../context/Context';
+import { getSellers } from '../../services';
 
-function Login() {
+function Admin() {
+  const [users, setUsers] = useState([]);
+  const {
+    token,
+    initializeUser,
+  } = useContext(Context);
+
+  console.log(users);
+  useEffect(() => {
+    initializeUser();
+    if (token) {
+      getSellers(token)
+        .then((response) => {
+          setUsers(response.users);
+        })
+        .catch((e) => console.log(e));
+    }
+  }, [initializeUser, setUsers, token]);
+
   return (
     <div>
       <Navbar />
@@ -13,4 +33,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Admin;
